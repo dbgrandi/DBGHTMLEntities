76e1b04d372010205937856e3adf061bce0e0c3d
David Grandinetti <dave@wegoto12.com>
2014-04-14 23:59:33 -0400

    not sure if I want the Pods dir checked in for this

--- Example/Pods/Pods-DBGHTMLEntitiesExampleTests-DBGHTMLEntities-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DBGHTMLEntitiesExampleTests_DBGHTMLEntities : NSObject
-@end
-@implementation PodsDummy_Pods_DBGHTMLEntitiesExampleTests_DBGHTMLEntities
-@end
--- Example/Pods/Pods-DBGHTMLEntitiesExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DBGHTMLEntitiesExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_DBGHTMLEntitiesExampleTests
-@end
